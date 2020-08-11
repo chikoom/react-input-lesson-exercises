f79abcc108edc927ba4511e54f8e572668108e5e
Chikoom <callforcopy@gmail.com>
2020-08-11 11:00:59 +0300

    input

--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -1,23 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Exercise2 extends Component {
-
-constructor() {
+  constructor() {
     super()
     this.state = {
-        name: "",
-        fruit: ""
+      name: '',
+      fruit: '',
     }
-}
-
-render() {
+  }
+  handleinput = e => {
+    const inputVal = e.target.value
+    const inputName = e.target.name
+    this.setState(
+      {
+        [inputName]: inputVal,
+      },
+      () => {
+        console.log(`${this.state.name} selected ${this.state.fruit}`)
+      }
+    )
+  }
+  render() {
     return (
-        <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
-        </div>
-    );
-}
+      <div>
+        <input
+          name='name'
+          id='name-input'
+          value={this.state.name}
+          onChange={this.handleinput}
+        />
+        <select
+          onChange={this.handleinput}
+          name='fruit'
+          id='select-input'
+          value={this.state.fruit}
+        >
+          <option value='Apple'>Apple</option>
+          <option value='Banana'>Banana</option>
+          <option value='Orange'>Orange</option>
+        </select>
+      </div>
+    )
+  }
 }
 
-export default Exercise2;
+export default Exercise2
