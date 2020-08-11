19c807165972212d4718678d9887be15f7927aa6
Chikoom <callforcopy@gmail.com>
2020-08-11 10:42:05 +0300

    EX1

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -1,24 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Exercise1 extends Component {
-
-    constructor() {
-        super()
-        this.state = {
-            name: "",
-            age: ""
-        }
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      age: '',
     }
+  }
+  handleInput = e => {
+    const fieldName = e.target.name
+    const fieldValue = e.target.value
+    this.setState({
+      [fieldName]: fieldValue,
+    })
+  }
+  alertMsg = () => {
+    alert(`Hello ${this.state.name}! You're ${this.state.age}`)
+  }
 
-    render() {
-        return (
-            <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input
+          id='name-input'
+          name='name'
+          value={this.state.name}
+          onChange={this.handleInput}
+        />
+        <input
+          id='age-input'
+          name='age'
+          value={this.state.age}
+          onChange={this.handleInput}
+        />
+        <button onClick={this.alertMsg}>Go to Bar</button>
+      </div>
+    )
+  }
 }
 
-export default Exercise1;
+export default Exercise1
